d4b1165408543c67723dfd55a2cbf72069883b66
Godmar Back <godmar@gmail.com>
2018-04-13 15:10:01 -0400

    use https instead of git for github.com

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,14 +5,14 @@
 #
 BASE=`pwd`
 test -d ${BASE}/deps || mkdir ${BASE}/deps
-git clone git@github.com:akheron/jansson.git
+git clone https://github.com/akheron/jansson.git
 (cd jansson;
     autoreconf -fi;
     ./configure --prefix=${BASE}/deps;
     make install
 )
 
-git clone git@github.com:benmcollins/libjwt.git
+git clone https://git@github.com/benmcollins/libjwt.git
 (cd libjwt;
     autoreconf -fi;
     env PKG_CONFIG_PATH=../deps/lib/pkgconfig:${PKG_CONFIG_PATH} ./configure --prefix=${BASE}/deps;
